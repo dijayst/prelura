58c1f3aed6b2279bb94e72803764aa4f158e39f8
Esther <dijayst@gmail.com>
2025-02-11 09:17:00 +0100

    inprogress

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}", // Ensure Tailwind scans all React components
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: "class",
   theme: {
     extend: {
       height: {
-        "screen-90": "10240vh", // Custom height for responsiveness
+        "screen-90": "1024vh", // Custom height for responsiveness
       },
     },
   },
