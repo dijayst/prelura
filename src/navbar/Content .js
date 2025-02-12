0e7553a45628d8d4e1b5912e329c4cf373ee027a
Esther <dijayst@gmail.com>
2025-02-12 10:02:44 +0100

    inprogress

--- src/navbar/Content .js
+++ src/navbar/Content .js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { Overview } from "./Data";
 import Barchart from "../Chart/Barchart";
 import Piechart from "../Chart/Piechart";
